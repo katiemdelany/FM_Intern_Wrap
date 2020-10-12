e9170b87e1f836e7b32652768c597a4ad6719e12
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2020-10-12 13:52:43 -0500

    Update hyb_wrap.py

--- hyb_wrap.py
+++ hyb_wrap.py
@@ -17,6 +17,7 @@ def check_arg(args=None):
             help = 'Run BWA pipe if targets are nucleotide sequences')
     parser.add_argument('--AA', action='store_true', dest='AA', default=False, 
             help = 'Run BLASTX pipe if targets are amino acid sequences')
+    #trim won't work yet - need universal path to trimmomatic (might not work out TBD)
     parser.add_argument('--trim', action='store_true', default=False,
             help = 'Clean fastq data using trimmomatic')
     parser.add_argument('--dataset', actoion = 'store_true', default = True,
@@ -27,14 +28,16 @@ args = check_arg(sys.argv[1:])
 
 
 def main():
-    clone_hybpiper = 'git clone https://github.com/mossmatters/HybPiper.git'
-    os.system(clone_hybpiper)
-## Get namelist.txt first
-## Needs to be in directory of dataset (added dataset as required user input)
+    ## Get namelist.txt first
+    ## Needs to be in directory of dataset (added dataset as required user input)
     dataset_path = args.dataset
     os.chdir(dataset_path)
     namelist_cmd = 'python3 getNameList.py'
     os.system(namelist_cmd)
+    
+    #Clones hybpiper into current directory
+    clone_hybpiper = 'git clone https://github.com/mossmatters/HybPiper.git'
+    os.system(clone_hybpiper)
  
 ## need to specify paths for each run eventually (mkdir --> chdir) 
     if args.trim:
