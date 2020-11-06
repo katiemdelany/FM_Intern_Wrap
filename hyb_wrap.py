38a9c38a0120f9f51653b11fc63b4eaf68f00c71
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2020-11-06 12:33:05 -0600

    Update hyb_wrap.py

--- hyb_wrap.py
+++ hyb_wrap.py
@@ -13,17 +13,15 @@ from os import path
 #KD- Check that it works for both (eventually)
 def check_arg(args=None):
     parser = argparse.ArgumentParser(description='Run Hybpiper for nucleotide or amino acid sequencing data')
-    parser.add_argument('--dna',action='store_true', dest='dna', default=False, 
-            help = 'Run BWA pipe if targets are nucleotide sequences')
-    parser.add_argument('--AA', action='store_true', dest='AA', default=False, 
-            help = 'Run BLASTX pipe if targets are amino acid sequences')
-    parser.add_argument('--assembly', action='store_true', dest='assembly',default =False,
-            help = 'run exonerate on full assemblies')
-    #trim won't work yet - need universal path to trimmomatic (might not work out TBD)
-    parser.add_argument('--trim', action='store_true', default=False,
-            help = 'Clean fastq data using trimmomatic')
-    parser.add_argument('--dataset', actoion = 'store_true', default = True,
-            help - 'Input path of test dataset')
+    parser.add_argument('--target_enrichment_data', action = 'store_true',default =False,
+            help = 'Input path of target enrichment data')
+    parser.add_argument('--assembly_data', action='store_true', dest='assembly',default =False,
+            help = 'Input path of assembly data')
+    parser.add_argument('--whole_genome_data', actoion = 'store_true', default = True,
+            help - 'Input path of whole genome sequence data.')
+    
+  #     parser.add_argument('--trim', action='store_true', default=False,
+ #           help = 'Clean fastq data using trimmomatic')
 
     return parser.parse_args(args)
 args = check_arg(sys.argv[1:])
@@ -32,9 +30,6 @@ args = check_arg(sys.argv[1:])
 def main():
     ## Get namelist.txt first
     ## Needs to be in directory of dataset (added dataset as required user input)
-    #Moves to dataset path
-    dataset_path = args.dataset
-    os.chdir(dataset_path)
 
     #Get namelist.txt from dataset directory
     namelist_cmd = 'python3 ../FM_Intern_Wrap/getNameList.py'
@@ -44,26 +39,9 @@ def main():
     os.system('cd ../')
     clone_hybpiper = 'git clone https://github.com/mossmatters/HybPiper.git'
     os.system(clone_hybpiper)
- 
-## need to specify paths for each run eventually (mkdir --> chdir)
-    if args.trim:
-        os.chdir("~/HybPiper/test_dataset/")
-        logging.info('Trimming input reads')
-        trim_cmd ='./trimmer.sh'
-        os.system(trim_cmd)
 
-    #if user input is dna
-    if args.dna:
-        logging.info("Creating new directory for dna target run")
-        dna_path = 'dna_run/'
-        os.mkdir(dna_path)
-        os.chdir(dna_path)
-        logging.info('Running DNA target script')
-        runDNAcmd = './run_DNA.sh'
-        os.system(runDNAcmd) #adjust DNA shell as needed
-
-    #if user input is AA 
-    if args.AA:
+    #if user input is target enrichment data
+    if args.target_enrichment_data:
         logging.info("Creating new directory for amino acid target run")
         aa_path = '~/HybPiper/test_dataset/aa_run/'
         os.mkdir(aa_path)
