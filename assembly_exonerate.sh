d7fd9f20f4e5da8d3e65004083ccf506ff62bb9b
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2020-11-08 15:11:42 -0600

    Update assembly_exonerate.sh

--- assembly_exonerate.sh
+++ assembly_exonerate.sh
@@ -1,4 +1,28 @@
 ## Need branch of pipeline for spades assembly input
 
+spades_path = $1
+target_path = $2
 
-os.chdir(path_to_assemblies)
+mkdir -p exonerate
+cd exonerate
+
+for i in $spades_path; do 
+	name=${i##*/}
+	~/git/HybPiper/exonerate_hits.py target_path $i/contigs.fasta --prefix $name
+done
+
+#make a genelist
+grep ">" target_path | sed s/^.*\-// > genelist.txt
+
+genelist=`cat genelist.txt`
+for i in $spades_path; do
+        name=${i##*/}
+        echo Working on $name
+                for gene in $genelist; do
+                        mkdir -p ./$name/$gene/$name/sequences/FNA
+                        cp ./$name/sequences/FNA/$gene.FNA ./$name/$gene/$name/sequences/FNA
+                        mkdir -p ./$name/$gene/$name/sequences/FAA
+                        cp ./$name/sequences/FAA/$gene.FAA ./$name/$gene/$name/sequences/FAA
+                done
+        echo Done with $name
+done
