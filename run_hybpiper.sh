10010b373a822d5bcc4a8b79781dc77225345aa0
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2020-11-10 08:31:42 -0600

    Create run_hybpiper.sh
    Bash script that runs hybpiper scripts from home directory. Scripts include reads_first, get_seq_length so far

---
+++ run_hybpiper.sh
@@ -0,0 +1,18 @@
+################################
+#run HyPiper with blastx option#
+################################
+target_file_path = $1
+path_to_dataset = $2
+
+git clone https://github.com/mossmatters/HybPiper.git
+
+mkdir -p hybpiper_blastx
+cd hybpiper_blastx
+
+#runnning HybPiper main script on user input dataset
+while read name;
+do ../HybPiper/reads_first.py -b target_file_path -r path_to_dataset/$name*.fastq --prefix $name --cpu 6
+done < ./namelist.txt
+
+#Visualizing Results
+python3 ../HybPiper/get_seq_lengths.py target_file_path namelist.txt aa > test_seq_lengths.txt
