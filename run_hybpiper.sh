3042ce42739fa0103c333223145fc409b53c0403
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2020-11-11 14:08:30 -0600

    Update run_hybpiper.sh

--- run_hybpiper.sh
+++ run_hybpiper.sh
@@ -15,5 +15,8 @@ while read name;
 do ../HybPiper/reads_first.py -b target_file_path -r path_to_dataset/$name*.fastq --prefix $name --cpu 6
 done < ./namelist.txt
 
+#Retrieve sequences
+python3 ../HybPiper/retrieve_sequences.py target_file_path . aa
+
 #Visualizing Results
 python3 ../HybPiper/get_seq_lengths.py target_file_path namelist.txt aa > test_seq_lengths.txt
