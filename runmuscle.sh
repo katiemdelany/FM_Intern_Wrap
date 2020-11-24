3dfe221c836d4c823bddeb50674815e07477f1c8
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2020-11-24 10:40:11 -0600

    Update runmuscle.sh

--- runmuscle.sh
+++ runmuscle.sh
@@ -5,9 +5,15 @@ for $f in *.FAA;
 do muscle -in $f -out $f.aligned.fas
 done 
 
+mkdir select_genes/
+
 for f in *.FAA.aligned.fas; do
 i = `cat $f | grep ">" | wc -l`
 echo $f has $i taxa
-
+#if [$i -ge 9 taxa]; then
+#cp $f select_genes/
+#echo copied $f with $i sequences
+#fi
+#done
 
 
