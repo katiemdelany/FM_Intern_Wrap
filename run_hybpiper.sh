5fb5c222094703b4471029c9cc1857f6493abb9a
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2020-12-15 10:53:02 -0600

    Update run_hybpiper.sh

--- run_hybpiper.sh
+++ run_hybpiper.sh
@@ -7,17 +7,19 @@ path_to_dataset = $2
 
 git clone https://github.com/mossmatters/HybPiper.git
 
-mkdir -p hybpiper_blastx
-cd hybpiper_blastx
+###
+#mkdir -p hybpiper_blastx
+#cd hybpiper_blastx
+###
 
 #running HybPiper main script on user input dataset
 #must have name list with no space at end
 while read name;
-do ~/FM_Intern_Wrap/HybPiper/reads_first.py -b target_file_path -r path_to_dataset/$name*.fastq --prefix $name 
+do ~/FM_Intern_Wrap/HybPiper/reads_first.py -b ${target_file_path} -r ${path_to_dataset}/$name*.fastq --prefix $name 
 done < ./namelist.txt
 
 #Retrieve sequences
-python3 ~/FM_Intern_Wrap/HybPiper/retrieve_sequences.py target_file_path . aa
+python3 ~/FM_Intern_Wrap/HybPiper/retrieve_sequences.py ${target_file_path} . aa
 
 #Visualizing Results
 #python3 ../HybPiper/get_seq_lengths.py target_file_path namelist.txt aa > test_seq_lengths.txt
