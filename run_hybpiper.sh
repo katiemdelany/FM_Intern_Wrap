d0009e1ae383d1e0ceba7c0844f039d3f37497ba
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2020-12-15 08:48:43 -0600

    Update run_hybpiper.sh

--- run_hybpiper.sh
+++ run_hybpiper.sh
@@ -13,11 +13,11 @@ cd hybpiper_blastx
 #running HybPiper main script on user input dataset
 #must have name list with no space at end
 while read name;
-do ../HybPiper/reads_first.py -b target_file_path -r path_to_dataset/$name*.fastq --prefix $name 
+do ~/FM_Intern_Wrap/HybPiper/reads_first.py -b target_file_path -r path_to_dataset/$name*.fastq --prefix $name 
 done < ./namelist.txt
 
 #Retrieve sequences
-python3 ../HybPiper/retrieve_sequences.py target_file_path . aa
+python3 ~/FM_Intern_Wrap/HybPiper/retrieve_sequences.py target_file_path . aa
 
 #Visualizing Results
 #python3 ../HybPiper/get_seq_lengths.py target_file_path namelist.txt aa > test_seq_lengths.txt
