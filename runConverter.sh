c8a1e63e6c92cf883f3163d7787e60f79080eb2a
Kathleen Delany <katiemdelany@gmail.com>
2021-01-04 11:01:29 -0600

    working script

--- runConverter.sh
+++ runConverter.sh
@@ -3,6 +3,6 @@
 for f in $(ls *.aligned.fas);
 do 
 	name=${f%%.aligned.fas}
-	~/FM_Intern_Wrap/fasTophy.py --in_fas $f --out_phy $name.aligned.phy
+        python3	~/FM_Intern_Wrap/fasTophy.py --in_fas $f --out_phy $name.aligned.phy
 
 done
