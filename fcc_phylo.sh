10733c371b84cebc173d233aa9be6a1057982861
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2021-01-22 11:59:40 -0600

    Update fcc_phylo.sh

--- fcc_phylo.sh
+++ fcc_phylo.sh
@@ -1,3 +1,11 @@
+#potential work around for finding programs
+#check FASconCAT
+#until [ -s $DIR_FASconCAT/FASconCAT*.pl ]
+#    do
+#      read -p "DIR_FASconCAT is not found. Please input its installation directory (absolute path, e.g. /home/zf/install/FASconCAT-G):      " DIR_FASconCAT
+#    done
+#echo "FASconCAT ...... OK"
+
 #change directory to .phy files
 #copy fasconcat-g into directory
 #run fasconcat-g
