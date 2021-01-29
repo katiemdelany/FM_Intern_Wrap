a8994ada9dccec003076b61dd584db9240189f33
Kathleen Delany <54127089+katiemdelany@users.noreply.github.com>
2021-01-29 10:34:22 -0600

    Update fcc_phylo.sh

--- fcc_phylo.sh
+++ fcc_phylo.sh
@@ -32,7 +32,8 @@ until [ $THREADS -gt 0 ]
 #change directory to .phy files
 #copy fasconcat-g into directory
 #run fasconcat-g
-perl FASconCAT_v1.11.pl
+# Need any other non-default parameters
+perl $DIR_FasconCAT/FASconCAT_*.pl
 
 #then use raxml
 raxmlHPC-PTHREADS -n homologs -s FcC_smatrix.phy -m GTRGAMMA -f a -p 194955 -x 12345 -# 100 -T 10
